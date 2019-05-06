6b54257c19c311ac908146cef51378c4c60fd607
Kilian Holzinger <kilian.holzinger@gmail.com>
2019-05-06 21:18:06 +0200

    Add workaround for unset revision var

--- build-toolchain.sh
+++ build-toolchain.sh
@@ -2,9 +2,12 @@
 
 set -euxo pipefail
 
+# workaround for https://github.com/ashwanthkumar/gocd-build-github-pull-requests/issues/133
+GRISP_SOFTWARE_REV=${GO_REVISION_GRISP_SOFTWARE:-$GO_SCM_GRISP_SOFTWARE_LABEL}
+
 cd grisp-software
 rm -rf /opt/grisp
-SHORT_SHA1=$(echo "$GO_REVISION_GRISP_SOFTWARE" | cut -c 1-10)
+SHORT_SHA1=$(echo "$GRISP_SOFTWARE_REV" | cut -c 1-10)
 TOOLCHAIN_BASE_PATH="/opt/grisp/grisp-software/grisp-base"
 TOOLCHAIN_PATH="$TOOLCHAIN_BASE_PATH/$SHORT_SHA1"
 GO_PWD=$PWD
