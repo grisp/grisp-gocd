dc620cc12282ebeb183aac2516708909a4337ea1
Kilian Holzinger <kilian.holzinger@gmail.com>
2019-05-15 16:54:01 +0200

    Upload all OTP versions

--- deploy-otp.sh
+++ deploy-otp.sh
@@ -2,10 +2,12 @@
 
 set -euxo pipefail
 
-ARCHIVENAME=$(basename otp/grisp_otp_build_*.tar.gz)
-COUNT=$(aws s3 ls s3://grisp/platforms/grisp_base/otp/"$ARCHIVENAME" | wc -l)
-if [[ $COUNT -eq 0 ]]; then
-    aws s3 cp otp/"$ARCHIVENAME" s3://grisp/platforms/grisp_base/otp/ --acl public-read
-else
-    echo File already on S3;
-fi > deploy_log 2>&1
+for otp in otp/grisp_otp_build_*.tar.gz; do
+    ARCHIVENAME=$(basename $otp)
+    COUNT=$(aws s3 ls s3://grisp/platforms/grisp_base/otp/"$ARCHIVENAME" | wc -l)
+    if [[ $COUNT -eq 0 ]]; then
+        aws s3 cp otp/"$ARCHIVENAME" s3://grisp/platforms/grisp_base/otp/ --acl public-read
+    else
+        echo File already on S3;
+    fi >> deploy_log 2>&1
+done
