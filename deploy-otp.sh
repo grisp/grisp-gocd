5413e414b137c4fb38993d6abefe7636fd161ace
Kilian Holzinger <kilian.holzinger@gmail.com>
2019-05-15 13:42:35 +0200

    Fix otp deploy

--- deploy-otp.sh
+++ deploy-otp.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-ARCHIVENAME=$(basename $PWD/grisp_otp_build_*.tar.gz)
-COUNT=$(aws s3 ls s3://grisp/platforms/grisp_base/otp/$ARCHIVENAME | wc -l)
+ARCHIVENAME=$(basename otp/grisp_otp_build_*.tar.gz)
+COUNT=$(aws s3 ls s3://grisp/platforms/grisp_base/otp/"$ARCHIVENAME" | wc -l)
 if [[ $COUNT -eq 0 ]]; then
-    aws s3 cp grisp_otp_build_*.tar.gz s3://grisp/platforms/grisp_base/otp/ --acl public-read
+    aws s3 cp otp/"$ARCHIVENAME" s3://grisp/platforms/grisp_base/otp/ --acl public-read
 else
     echo File already on S3;
-fi 2>&1 > /tmp/deploy_log
+fi > deploy_log 2>&1
