dcb91dbb242e48a3bd148f100ba32e4ad0f021ba
Kilian Holzinger <kilian.holzinger@gmail.com>
2019-05-15 17:02:27 +0200

    Disable pipefail for failing pipe

--- deploy-otp.sh
+++ deploy-otp.sh
@@ -4,7 +4,9 @@ set -euxo pipefail
 
 for otp in otp/grisp_otp_build_*.tar.gz; do
     ARCHIVENAME=$(basename $otp)
+    set +o pipefail
     COUNT=$(aws s3 ls s3://grisp/platforms/grisp_base/otp/"$ARCHIVENAME" | wc -l)
+    set -o pipefail
     if [[ $COUNT -eq 0 ]]; then
         aws s3 cp otp/"$ARCHIVENAME" s3://grisp/platforms/grisp_base/otp/ --acl public-read
     else
