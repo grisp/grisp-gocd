8fde71ecb14b5002d8dcc4e63bd32a535e56a508
Kilian Holzinger <kilian.holzinger@gmail.com>
2019-07-05 18:06:49 +0200

    Commit work in progress of python version

--- pipelines/create_pipelines.py
+++ pipelines/create_pipelines.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import os
 import jinja2
 import json
@@ -7,92 +6,105 @@ import itertools
 
 config_repo_dir = "../grisp-gocd-config"
 
-all_otp_versions = [ "20.2", "21.0-rc1", "21.0" ]
+all_otp_versions = [ "20.2", "21.0-rc1", "21.0", "22.0" ]
 default_otp_version = [ "21.0" ]
+all_material_types = [ "master-material", "fb-material", "pr-material" ]
 
-grisp_matrix={
-    "grisp": {
-        "versions": all_otp_versions,
-        "grisp_material": True,
-        "rebar3_grisp_material": False,
-        "grisp_tools_material": False,
-        "fetch_toolchain": False,
-        "build_otp" : True
-    },
-    "new-toolchain": {
-        "versions": all_otp_versions,
-        "grisp_material": False,
-        "rebar3_grisp_material": False,
-        "grisp_tools_material": False,
-        "fetch_toolchain": True,
-        "build_otp" : True
+def product_dict(**kwargs):
+    keys = kwargs.keys()
+    vals = kwargs.values()
+    list_vals = [ i if isinstance(i, (list,)) else [i] for i in vals ]
+    for instance in itertools.product(*list_vals):
+        yield dict(zip(keys, instance))
+
+def get_grisp_pipelines():
+    grisp_matrix_config={
+        "grisp": {
+            "version": all_otp_versions,
+            "grisp_material": all_material_types,
+            "rebar3_grisp_material": False,
+            "grisp_tools_material": False,
+            "fetch_toolchain": False,
+            "build_otp" : True,
+            "name": lambda otp, grisp, rebar3_grisp, grisp_tools, build : "grisp-"+grisp+"-otp-"+otp,
+            "push_otp": True
+        },
+        "new-toolchain": {
+            "version": all_otp_versions,
+            "grisp_material": False,
+            "rebar3_grisp_material": False,
+            "grisp_tools_material": False,
+            "fetch_toolchain": True,
+            "build_otp" : True,
+            "name": lambda otp, grisp, rebar3_grisp, grisp_tools, build : "new-toolchain-otp-"+otp,
+            "push_toolchain": True
+        },
+        "rebar3_grisp": {
+            "version": default_otp_version,
+            "grisp_material": [],
+            "grisp_dep": ["master", "hex"],
+            "rebar3_grisp_material": all_material_types,
+            "grisp_tools_material": [],
+            "grisp_tools_dep": ["master", "hex"],
+            "fetch_toolchain": False,
+            "build_otp": [False, True],
+            "name": lambda otp, grisp, rebar3_grisp, grisp_tools, build :
+            "rebar3-grisp"+rebar3_grisp+"-grisp-"+grisp+"-tools-"+grisp_tools+"-build-"+str(build).lower()+"-otp-"+otp
+        },
+        "grisp_tools": {
+            "version": default_otp_version,
+            "grisp_material": [],
+            "grisp_dep": ["master", "hex"],
+            "rebar3_grisp_material": [],
+            "rebar3_grisp_dep": ["master", "hex"],
+            "grisp_tools_material": all_material_types,
+            "fetch_toolchain": False,
+            "build_otp": [False, True],
+            "name": lambda otp, grisp, rebar3_grisp, grisp_tools, build :
+            "grisp-tools-"+rebar3_grisp+"-grisp-"+grisp+"-tools-"+grisp_tools+"-build-"+str(build).lower()+"-otp-"+otp
+        }
+
     }
-}
+    grisp_matrix = {}
+    for k, v in grisp_matrix_config.items():
+        for i in (product_dict(**v)):
+            name = i['name'](i['version'],
+                             i['grisp_material'],
+                             i['rebar3_grisp_material'],
+                             i['grisp_tools_material'],
+                             i['build_otp'])
+            i['pipeline_name'] = name
+            grisp_matrix[name] = i
 
+    return grisp_matrix
+    #print(str(grisp_matrix))
 
-def create_rebar3_conf(grisp, tools, build):
-    return { "rebar3_grisp_"+grisp[0]+"_tools_"+tools[0]+"_build_"+str(build[0]) : {
-        "versions": default_otp_version,
-        "grisp_material": grisp[1],
-        "rebar3_grisp_material": True,
-        "grisp_tools_material": tools[1],
-        "fetch_toolchain": False,
-        "build_otp": build[1]
-    }}
-
-# (PIPELINE_NAME, MATERIAL_VALUE)
-grisp_combinations = [ ("master", True), ("hex", False) ]
-tools_combinations = [ ("master", True), ("hex", False) ]
-build_combinations = [ ("true", True), ("false", False) ]
-
-rebar3_combinations = list(itertools.product(grisp_combinations, tools_combinations, build_combinations))
-
-for combination in rebar3_combinations:
-    conf = create_rebar3_conf(*combination)
-    for name, parameters in conf.items():
-        grisp_matrix[name] = parameters
-
-env = jinja2.Environment(loader = jinja2.FileSystemLoader(os.path.abspath('.')))
-
-def create_pipeline(template, pipeline_name, data):
-    print(data)
+def render_pipeline(template, pipeline_name, data):
+#    print(data)
     template = env.get_template(os.path.join('pipelines', 'templates', template))
     pipeline_json = template.render(**data)
     with open(os.path.join(config_repo_dir, pipeline_name + ".gopipeline.json"), "w") as pipeline:
         pipeline.write(pipeline_json)
     json.loads(pipeline_json) # check if it's valid json
 
-def get_grisp_pipeline_name(pipeline_type, otp_version):
-    return pipeline_type+"-otp-"+otp_version
-
-# grisp pipelines:
-for pipeline_type, item in grisp_matrix.items():
-    for v in item["versions"]:
-        pipeline_name = get_grisp_pipeline_name(pipeline_type, v)
-        create_pipeline('grisp.j2.gopipeline.json',
-                        pipeline_name,
-                        {
-                            "pipeline_name" : pipeline_name,
-                            "erlang_version" : v,
-                            "grisp_material" : item["grisp_material"],
-                            "rebar3_grisp_material" : item["rebar3_grisp_material"],
-                            "grisp_tools_material" : item["grisp_tools_material"],
-                            "fetch_toolchain" : item["fetch_toolchain"],
-                            "build_otp" : item["build_otp"]
-                        }
-        )
-
-create_pipeline('deploy-otp-to-s3.j2.gopipeline.json',
+env = jinja2.Environment(loader = jinja2.FileSystemLoader(os.path.abspath('.')))
+grisp_matrix = get_grisp_pipelines()
+for k, v in grisp_matrix.items():
+    print(k,v)
+    print('\n')
+    #render_pipeline('grisp.j2.gopipeline.json', k, v)
+exit()
+render_pipeline('deploy-otp-to-s3.j2.gopipeline.json',
                 'deploy-otp-to-s3',
                 # all otp versions as upstream pipelines
                 { 'upstream_pipelines' :
-                  [ get_grisp_pipeline_name('grisp', v) for v in grisp_matrix["grisp"]["versions"] ]
+                  [ v['pipeline_name'] for v in grisp_matrix.values() if 'push_otp' in v and v['push_otp'] ]
                 }
 )
 
-create_pipeline('deploy-toolchain-to-s3.j2.gopipeline.json',
+render_pipeline('deploy-toolchain-to-s3.j2.gopipeline.json',
                 'deploy-toolchain-to-s3',
                 { 'upstream_pipelines' :
-                  [ get_grisp_pipeline_name('new-toolchain', v) for v in grisp_matrix['new-toolchain']['versions'] ]
+                  [ v['pipeline_name'] for v in grisp_matrix.values() if 'push_toolchain' in v and v['push_toolchain'] ]
                 }
 )
