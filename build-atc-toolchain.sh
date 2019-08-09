cbbb431821485f5fa2b54f8e055d486901e8f741
Kilian Holzinger <kilian.holzinger@gmail.com>
2019-08-09 12:48:12 +0200

    Board type as CLI argument

--- build-atc-toolchain.sh
+++ build-atc-toolchain.sh
@@ -10,7 +10,7 @@ GRISP_SOFTWARE_REV=$(git rev-parse HEAD)
 
 rm -rf /opt/grisp
 SHORT_SHA1=$(echo "$GRISP_SOFTWARE_REV" | cut -c 1-10)
-TOOLCHAIN_BASE_PATH="/opt/grisp/grisp-software/$1"
+TOOLCHAIN_BASE_PATH=/opt/grisp/grisp-software/"$1"
 TOOLCHAIN_PATH="$TOOLCHAIN_BASE_PATH/$SHORT_SHA1"
 mkdir -p "$TOOLCHAIN_BASE_PATH"
 ln -s "$GO_PWD"/grisp-software "$TOOLCHAIN_PATH"
@@ -20,7 +20,7 @@ make all
 echo "OK" > rtems-source-builder/rtems/rsb-report-ok
 
 cd /
-ARCHIVENAME=atc_toolchain_arm-rtems5_Linux_$GRISP_SOFTWARE_REV.tar.gz
+ARCHIVENAME="$1"_toolchain_arm-rtems5_Linux_$GRISP_SOFTWARE_REV.tar.gz
 tar -czf "$GO_PWD"/"$ARCHIVENAME" "$TOOLCHAIN_PATH"/rtems-install
 echo "$GRISP_SOFTWARE_REV" > "$GO_PWD"/TOOLCHAIN_REVISION
 rm -rf /opt/grisp
