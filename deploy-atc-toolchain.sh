f2f991a5bd2983ff5bcca0128ba9b6dcb77d8163
Kilian Holzinger <kilian.holzinger@gmail.com>
2019-08-12 16:56:33 +0200

    Fix wrong archive name

--- deploy-atc-toolchain.sh
+++ deploy-atc-toolchain.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-ARCHIVENAME=$(basename toolchain/"$1"-toolchain*.tar.gz)
+ARCHIVENAME=$(basename toolchain/"$1"_toolchain*.tar.gz)
 set +o pipefail
 COUNT=$(aws s3 ls s3://grisp/platforms/"$1"/toolchain/"$ARCHIVENAME" | wc -l)
 set -o pipefail
@@ -10,7 +10,7 @@ set -o pipefail
 
 if [[ $COUNT -eq 0 ]]; then
     aws s3 cp toolchain/"$ARCHIVENAME" s3://grisp/platforms/"$1"/toolchain/ --acl public-read
-    aws s3 cp toolchain/"$ARCHIVENAME" s3://grisp/platforms/"$1"/toolchain/"$1"-toolchain_arm-rtems5_Linux_latest.tar.gz --acl public-read
+    aws s3 cp toolchain/"$ARCHIVENAME" s3://grisp/platforms/"$1"/toolchain/"$1"_toolchain_arm-rtems5_Linux_latest.tar.gz --acl public-read
 else
     echo File already on S3
 fi > deploy_log 2>&1
