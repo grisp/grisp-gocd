9c500237880bb737d8509b0cd0a3db764b1e8199
Kilian Holzinger <kilian.holzinger@gmail.com>
2019-08-19 11:15:34 +0200

    Add atc deploy script

---
+++ deploy-atc-otp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+for otp in otp/grisp_otp_build_*.tar.gz; do
+    ARCHIVENAME=$(basename $otp)
+    set +o pipefail
+    COUNT=$(aws s3 ls s3://grisp/platforms/atc/otp/"$ARCHIVENAME" | wc -l)
+    set -o pipefail
+    if [[ $COUNT -eq 0 ]]; then
+        aws s3 cp otp/"$ARCHIVENAME" s3://grisp/platforms/atc/otp/ --acl public-read
+    else
+        echo File already on S3;
+    fi >> deploy_log 2>&1
+done
